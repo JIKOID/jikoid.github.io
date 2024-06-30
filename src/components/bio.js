f1827f3ad362a82eb1dfba687ecf052b7080f5e9
jaynamm <jhnam@eazel.net>
2024-07-01 00:33:26 +0900

    Chore: fasebook 링크 제거

--- src/components/bio.js
+++ src/components/bio.js
@@ -28,7 +28,7 @@ const Bio = () => {
 
   // Set these values by editing "siteMetadata" in gatsby-config.js
   const author = data.site.siteMetadata?.author
-  const social = data.site.siteMetadata?.social
+  // const social = data.site.siteMetadata?.social
 
   return (
     <div className="bio">
@@ -44,11 +44,12 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
+          <strong>{author.name}</strong>
+          <p>{author?.summary || null}</p>
+          <p>{` `}</p>
+          {/* <a href={`https://twitter.com/${social?.twitter || ``}`}>
             You should follow them on Twitter
-          </a>
+          </a> */}
         </p>
       )}
     </div>

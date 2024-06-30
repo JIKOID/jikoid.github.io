1ff83f17cf805a7c8543dcbcda300d52ccccd890
jaynamm <jhnam@eazel.net>
2024-07-01 00:33:48 +0900

    Feature: article 사이에 라인 추가

---
+++ src/components/horizontal_line.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+export default function HorizontalLine() {
+    return (
+        <div style={{
+            width: "100%",
+            textAlign: "center",
+            borderBottom: "1px solid #aaa",
+            lineHeight: "0.1em",
+            margin: "10px 0 20px",
+        }} />
+    )
+}

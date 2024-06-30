dbbc122f015998534d8efd1b92bb01cf57b8a46a
jaynamm <jhnam@eazel.net>
2024-07-01 02:00:40 +0900

    Chore: summary 수정

--- blog-config.js
+++ blog-config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: "제이로그",
-    summary: "데이터 엔지니어",
+    summary: "어제보다 더 나은 오늘이 되고 싶은 데이터 엔지니어",
     desciption: "제이로그",
     author: "남제이",
     siteUrl: "https://jikoid.github.io/",

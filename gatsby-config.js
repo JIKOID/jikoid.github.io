dec085d0c22f3f9d107b32cb4b9e6d8fc4f1c1f9
jaynamm <jhnam@eazel.net>
2024-07-03 18:51:31 +0900

    fix: categories 제거

--- gatsby-config.js
+++ gatsby-config.js
@@ -107,7 +107,6 @@ module.exports = {
                   frontmatter {
                     title
                     date
-                    categories
                   }
                 }
               }

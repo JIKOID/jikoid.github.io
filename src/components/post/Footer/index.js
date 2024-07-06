82bc55d369e6d8d1f50efe88bfe0a8983c2cdbeb
jaynamm <jhnam@eazel.net>
2024-07-07 03:26:37 +0900

    Fix: 컴포넌트 대소문자 구분 에러

---
+++ src/components/post/Footer/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+
+import Bio from "../../Bio"
+
+export default function Footer() {
+    return (
+        <footer>
+            <Bio />
+        </footer>
+    )
+};

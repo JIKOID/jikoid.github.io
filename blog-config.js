9400b4c49f12503fb38b7f7a088ef43fd0536ff4
jaynamm <jhnam@eazel.net>
2024-07-16 16:57:06 +0900

    Config: 블로그 설정 변경

--- blog-config.js
+++ blog-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    title: "제이로그",
+    title: "JIKOID",
     summary: "공부하고 정리하는 블로그",
-    desciption: "제이로그",
+    description: "천천히 그리고 꾸준히",
     author: "남제이",
     siteUrl: "https://jikoid.github.io",
 }

ba3c35d4f64d912086df5a959b5ed9685d77dd8b
jaynamm <jhnam@eazel.net>
2024-07-23 18:53:02 +0900

    Style: footer 하단 고정

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -5,41 +5,13 @@ import Footer from "./Footer"
 
 import * as styles from "./Layout.module.scss"
 
-import Headroom from "headroom.js"
-import HeaderHambuger from "./HeaderHamburger"
-
-
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-
-  const headerRef = React.useRef();
-
-  React.useEffect(() => {
-    const headerElement = headerRef.current;
-
-    if (headerElement) {
-      try {
-        const headroom = new Headroom(headerElement);
-        headerElement.init();
-
-        // Log success message
-        console.log('Headroom initialized successfully on:', headerElement);
-      } catch (error) {
-        // Catch and log any errors during initialization
-        console.error('Failed to initialize Headroom:', error);
-      }
-    } else {
-      // Log if the header element is not found
-      console.error('Header element not found');
-    }
-  }, []);
   
   return (
-    <>
-      <Header ref={headerRef} location={location} title={title} />
-        <HeaderHambuger />
-
+    <div id="wrap">
+      <Header location={location} title={title} />
         <div className={styles.container}>
           {/* Sidebar */}
           <Sidebar location={location} />
@@ -49,8 +21,8 @@ const Layout = ({ location, title, children }) => {
             <main >{children}</main>
           </div>
         </div>
-      <Footer />
-    </>
+        <Footer />
+    </div>
   )
 }
 

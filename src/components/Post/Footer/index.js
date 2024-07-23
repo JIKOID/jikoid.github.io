c3d19953fb54f202c5c587b72738e7c0c22e7405
jaynamm <jhnam@eazel.net>
2024-07-23 19:08:31 +0900

    Style: 포스트 내 header 위, 아래 margin 추가

--- src/components/Post/Footer/index.js
+++ src/components/Post/Footer/index.js
@@ -5,7 +5,8 @@ import Bio from "../../Bio"
 export default function Footer() {
     return (
         <footer>
-            <Bio />
+            {/* <Bio /> */}
+            무엇이든 궁금한 것이 있으시다면 아래에 댓글로 남겨주세요 🙏
         </footer>
     )
 };

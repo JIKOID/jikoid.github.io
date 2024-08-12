d60b498936cae90dcd1dbe5fa0218debc528f713
jaynamm <polky0421@gmail.com>
2024-08-12 15:00:01 +0900

    Refactor: 다크 모드 애니메이션 추가 (#22)

--- src/components/DarkModeToggle/index.js
+++ src/components/DarkModeToggle/index.js
@@ -10,25 +10,30 @@ const ThemeIcon = {
 export default function DarkModeToggle() {
     const [theme, setTheme] = useState(null);
 
-	let isDarkMode = false;
-	if (typeof window !== 'undefined')
-		isDarkMode = theme === window.__DARK;
+    useEffect(() => {
+		setTheme(window.__theme);
+	}, []);
+
+    const isDarkMode = theme === window.__DARK;
 
 	const onClickDarkModeButton = useCallback(() => {
 		const newTheme = isDarkMode ? window.__LIGHT : window.__DARK;
 		window.__setTheme(newTheme);
 		setTheme(newTheme);
+
+         // Trigger rotation animation
+        const button = document.querySelector('.dark_mode_toggle button');
+        button.classList.add('rotate');
+
+        // Remove the class after the animation completes
+        setTimeout(() => {
+            button.classList.remove('rotate');
+        }, 500); // Match this duration with the CSS animation duration
 	}, [isDarkMode]);
 
-	useEffect(() => {
-		setTheme(window.__theme);
-	}, []);
-
-	if (!theme) {
-		return null;
-	}
-    if (theme === null) {
-        return null;
+     // 훅이 조건적으로 호출되지 않도록 보장한 후 렌더링 조건 설정
+     if (theme === null) {
+        return null; // 초기 테마 설정이 완료되기 전까지는 렌더링하지 않음
     }
 
     return (

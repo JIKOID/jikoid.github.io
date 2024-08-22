2da2485c5b0368423d086eb738c6afe273052ba7
jaynamm <jhnam@eazel.net>
2024-08-22 16:18:47 +0900

    Sytle: hits 를 footer 로 이동

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,11 +1,18 @@
 import * as React from "react"
+import "./Footer.scss"
 
 export default function Footer() {
     return (
         <footer>
-            © {new Date().getFullYear()} Jaynam. Built with 
-            {` `}
-            <a href="https://www.gatsbyjs.com">Gatsby</a>
+            <div className="footer_copylight">
+                © {new Date().getFullYear()} Jaynam. Built with 
+                {` `}
+                <a href="https://www.gatsbyjs.com">Gatsby</a>
+            </div>
+
+            <div className="footer_hits">
+                <img src="https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fjikoid.github.io%2Fhit-counter&count_bg=%23000000&title_bg=%23000000&icon=macys.svg&icon_color=%23E7E7E7&title=visitors&edge_flat=false"/>
+            </div>
         </footer>
     )
 }
